847509cd6c6556c8a1d0d8cc7da35636bd3e48d9
Satyarth Praveen <satyarth934@gmail.com>
2019-09-27 21:57:38 +0000

    Removing the lib.hpp file and emptying the main() function. No execution of the PID class is needed for now.

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,16 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ *  @copyright (c) 2019 Satyarth Praveen, Ishan Patel
+ *  @file    main.cpp
+ *  @author  Satyarth Praveen, Ishan Patel
+ *
+ *  @brief Using the PID class to compute the output
+ *  given the target setpoint and actual velocity.
+ *
+ */
 
-int main()
-{
-    dummy();
-    return 0;
+#include <iostream>
+#include <PID.hpp>
+
+int main() {
+  return 0;
 }
