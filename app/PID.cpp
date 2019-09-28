3288611a79846b7a34bfc9a03c4172daf4d59fd6
charan <pvcharankarthikeyan@gmail.com>
2019-09-28 19:10:11 -0400

    Added the Authors and added compute function implementation

--- app/PID.cpp
+++ app/PID.cpp
@@ -1,15 +1,24 @@
 /**
  *  @copyright (c) 2019 Satyarth Praveen, Ishan Patel
  *  @file    PID.cpp
- *  @author  Satyarth Praveen (driver), Ishan Patel (navigator)
+ *  @author Part 1:  Satyarth Praveen (driver), Ishan Patel (navigator)
+ *  @author Part 2:  Charan Karthikeyan (driver), Corbyn Yhap (navigator)
  *
  *  @brief Source file for the PID class and its members.
  *
+ *
  */
+
 
 #include <PID.hpp>
 
 double PID::compute(double targetSetpoint, double actualVelocity) {
   // enter your code here
-  return 0;
+  double dt = 1;
+  double error = targetSetpoint-actualVelocity;
+  double propotional = Kp*error;
+  double integral = Ki*error*dt;
+  double differential = Kd*error/dt;
+
+  return propotional + integral + differential;
 }
